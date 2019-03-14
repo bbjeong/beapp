b42dc99f052dabd0952b4a5aced2c7d20381be9a
infiduk <infi_duk@naver.com>
2019-03-14 19:51:30 +0900

    Update SimpleToken123.sol

--- SmartContract/contracts/SimpleToken123.sol
+++ SmartContract/contracts/SimpleToken123.sol
@@ -13,11 +13,23 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 contract SimpleToken123 is ERC20, ERC20Detailed {
     uint8 public constant DECIMALS = 18;
     uint256 public constant INITIAL_SUPPLY = 100000 * (10 ** uint256(DECIMALS));
+    
+    mapping(address => uint) public balancesUser;
 
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public ERC20Detailed("SimpleToken123", "SIM123", DECIMALS) {
+    constructor () public ERC20Detailed("SimpleToken123", "SIM12", DECIMALS) {
         _mint(msg.sender, INITIAL_SUPPLY);
+    }
+    
+    function balanceOfUser(address account) public view returns (uint256) {
+        return balancesUser[account];
+    }
+    
+    function transfer(address to, uint256 value) public returns (bool) {
+        _transfer(msg.sender, to, value);
+        balancesUser[to] = balancesUser[to] + value;
+        return true;
     }
 }
