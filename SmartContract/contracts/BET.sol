483a47777024c40ccb4533e39a802c293427c65a
infiduk <infi_duk@naver.com>
2019-04-01 13:44:08 +0900

    Add files via upload

---
+++ SmartContract/contracts/BET.sol
@@ -0,0 +1,37 @@
+pragma solidity >=0.5.0;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+
+
+/**
+ * @title BET
+ * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
+ * Note they can later distribute these tokens as they wish using `transfer` and other
+ * `ERC20` functions.
+ */
+ contract BET is ERC20, ERC20Detailed {
+    uint8 public constant DECIMALS = 18;
+    uint256 public constant INITIAL_SUPPLY = 100000 * (10 ** uint256(DECIMALS));
+    address owner;
+    
+    mapping(address => uint) public balancesUser;
+
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
+    constructor () public ERC20Detailed("BehavioralEconomicsToken", "BET", DECIMALS) {
+        _mint(msg.sender, INITIAL_SUPPLY);
+        owner = msg.sender;
+    }
+    
+    function balanceOfUser(address account) public view returns (uint256) {
+        return balancesUser[account];
+    }
+    
+    function transfer(address to, uint256 value) public returns (bool) {
+        _transfer(owner, to, value);
+        balancesUser[to] = balancesUser[to] + value;
+        return true;
+    }
+}
--- SmartContract/contracts/BeApp.sol
+++ SmartContract/contracts/BeApp.sol
@@ -1,43 +1,95 @@
 pragma solidity >=0.5.0;
 
-import "./SimpleToken123.sol";
+import "./BET.sol";
 
-contract BeApp is SimpleToken123 {
+contract BeApp is BET {
     
     uint public userIndex;
-    
+
     mapping(address => mapping(uint => User)) userInfo;
     mapping(string => uint) userEmailToId;
     mapping(uint => address) userIdToInfo;
+    mapping(uint => Answer) answerValue;
+    mapping(address => uint) userAddressToId;
+    
+    struct Answer2 { // string 섞인 Answer
+        uint answer1;
+        string answer2;
+        string answer3;
+        uint answer4;
+        uint answer5;
+        uint answer6;
+        uint answer7;
+        uint answer8;
+        uint answer9;
+        uint answer10;
+        string answer11;
+    }
+    
+    struct Answer {
+        uint answer1;
+        uint answer2;
+        uint answer3;
+        uint answer4;
+        uint answer5;
+        uint answer6;
+        uint answer7;
+        uint answer8;
+        uint answer9;
+        uint answer10;
+        uint answer11;
+    }
     
     struct User {
         uint id;
         uint age;
+        uint amountBET;
         string email;
         string ipfsValue;
         bool gender;
         address account;
     }
     
-    event NewUser(uint _userId, uint _age, string _email, string _ipfsValue, bool _gender, address _account);
+    event NewUser(uint _userId, uint _age, uint _amountBET, string _email, string _ipfsValue, bool _gender, address _account);
+    event NewAnswer(uint _amountBET, uint _answer1, uint _answer2, uint _answer3, uint _answer4, uint _answer5, uint _answer6, uint _answer7, uint _answer8, uint _answer9, uint _answer10, uint _answer11);
+    // event NewAnswer(uint _amountBET, uint _answer1, string _answer2, string _answer3, uint _answer4, uint _answer5, uint _answer6, uint _answer7, uint _answer8, uint _answer9, uint _answer10, string _answer11);
 
     constructor() public {
         userIndex = 0;
     }
     
-    function addUser(uint _age, string memory _email, string memory _ipfsValue, bool _gender) public {
+    function addUser(uint _age, string memory _email, bool _gender) public {
         userIndex += 1;
-        User memory user = User(userIndex, _age, _email, _ipfsValue, _gender, msg.sender);
+        User memory user = User(userIndex, _age, 0, _email, "", _gender, msg.sender);
         userInfo[msg.sender][userIndex] = user;
         userEmailToId[_email] = userIndex;
         userIdToInfo[userIndex] = msg.sender;
+        userAddressToId[msg.sender] = userIndex;
         balancesUser[msg.sender] = 0;
-	emit NewUser(userIndex, _age, _email, _ipfsValue, _gender, msg.sender);
+        emit NewUser(userIndex, _age, 0, _email, "", _gender, msg.sender);
     }
     
-    function getUser(string memory _userEmail) public view returns (uint, uint, string memory, string memory, bool, address) {
+    function addAnswer(uint _amountBET, string memory _ipfsValue, uint _answer1, uint _answer2, uint _answer3, uint _answer4, 
+        uint _answer5, uint _answer6, uint _answer7, uint _answer8, uint _answer9, uint _answer10, uint _answer11) public {
+        Answer memory answer = Answer(_answer1, _answer2, _answer3, _answer4, _answer5, _answer6, _answer7, _answer8, _answer9, _answer10, _answer11);
+        User memory user = userInfo[msg.sender][userAddressToId[msg.sender]];
+        answerValue[userIndex] = answer;
+        user.amountBET = _amountBET;
+        user.ipfsValue = _ipfsValue;
+        userInfo[msg.sender][userIndex] = user;
+        transfer(msg.sender, user.amountBET);
+        emit NewUser(user.id, user.age, user.amountBET, user.email, user.ipfsValue, user.gender, msg.sender);
+        emit NewAnswer(_amountBET, _answer1, _answer2, _answer3, _answer4, _answer5, _answer6, _answer7, _answer8, _answer9, _answer10, _answer11);
+        }
+    
+    function getUser(string memory _userEmail) public view returns (uint, uint, uint, string memory, string memory, bool, address) {
         User memory user = userInfo[userIdToInfo[userEmailToId[_userEmail]]][userEmailToId[_userEmail]];
-        // User memory user = userInfo[userIdInAccount[_userId]][_userId];
-        return (user.id, user.age, user.email, user.ipfsValue, user.gender, user.account);
+        return (user.id, user.age, user.amountBET, user.email, user.ipfsValue, user.gender, user.account);
+    }
+    
+    
+    function getAnswer(uint _userIndex) public view returns (uint, uint, uint, uint, uint, uint, uint, uint, uint, uint, uint) {
+        Answer memory answer = answerValue[_userIndex];    
+        return (answer.answer1, answer.answer2, answer.answer3, answer.answer4, answer.answer5, answer.answer6, answer.answer7, answer.answer8, answer.answer9, answer.answer10, answer.answer11);
     }
 }
